0f411497894348b3bd5a8517191398bb95955628
TristanDaniels <tristandaniels07@gmail.com>
2022-08-26 11:03:59 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
   <router-view />
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
